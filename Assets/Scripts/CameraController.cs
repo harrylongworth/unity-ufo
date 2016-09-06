950d151d3049f861983df8c8512d6aea6b863383
harrylongworth <harry@harrylongworth.com>
2016-09-06 16:43:53 +1000

    Initial Upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+	
+		offset = transform.position - player.transform.position;
+
+	}
+
+	void LateUpdate () {
+		// LateUpdate runs after all items in update have been processed (e.g. player has moved)
+
+		transform.position = player.transform.position + offset;
+	
+	} // LateUpdate
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public SimpleTouchPad touchPad;
+	public SimpleTouchPad touchPad2;
+
+	private Rigidbody2D rb2d;
+
+
+	// Use this for initialization
+	void Start () {
+
+		rb2d = GetComponent<Rigidbody2D>();
+	
+	} 
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		// TouchPad
+		Vector2 direction = touchPad.GetDirection ();
+		rb2d.AddForce (direction*speed);
+
+		Vector2 direction2 = touchPad2.GetDirection ();
+		rb2d.AddForce (direction2*speed);
+
+		// Keyboard
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		rb2d.AddForce (movement*speed);
+
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other) 
+	{
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{
+			other.gameObject.SetActive (false);
+		}
+		
+	}
+}
