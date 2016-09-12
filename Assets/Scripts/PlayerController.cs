9b721e6671dc5d2db46043eeb8b40ff3e3ab0a53
oatsea <harry@oatsea.org>
2016-09-12 13:30:06 +1000

    Enable multiple Player Sprites
    Multiple player sprites with ability to either select at random
    (default) or set by Sprite name if setSprite enabled.
    
    Includes additional Player Sprite artwork and original unity UFO

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public Sprite[] sprites;
+	public int spriteID = 0;
+	public bool setSprite = false;
 	public float speed;
 	public float acceleration;
 	public float rotationSpeed;
@@ -39,6 +42,30 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		//Set Sprites
+		Sprite currentSprite;
+
+
+
+		if (setSprite) {
+			if (spriteID < (sprites.Length)) {			
+				currentSprite = sprites [spriteID];
+			} else {
+				currentSprite = sprites [0];
+			}
+
+		} else {
+			// select random sprite
+			int randomSpriteID = (int) Mathf.Round(Random.Range(0,sprites.Length));
+			currentSprite = sprites [randomSpriteID ];
+		}
+
+
+		var renderer = GetComponent<SpriteRenderer> ();
+
+		renderer.sprite = currentSprite;
+
 
 		//Damage
 		playerDamage = 0;
