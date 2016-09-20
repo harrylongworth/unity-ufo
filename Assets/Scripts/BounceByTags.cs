f84fc46d31558b2fb2cb6ed2e0dd38ace4b641b5
oatsea <harry@oatsea.org>
2016-09-21 08:27:46 +1000

    Modify Player Icons

--- Assets/Scripts/BounceByTags.cs
+++ Assets/Scripts/BounceByTags.cs
@@ -35,23 +35,11 @@ public class BounceByTags : MonoBehaviour {
 						if (other.gameObject.CompareTag (bounceByTags [i])) {
 							// Bounce! 
 
-							// Debug.Log("Bouncing "+tag+ " off "+other.tag );
-
-							transform.Rotate (0, 0, 180);
-							//Debug.Log ("Bounced " + name +" off "+other.name);
-
-							float radianAngle = -1*(Mathf.Deg2Rad * (transform.eulerAngles.z-360.0f+Random.Range(0,90)));
-							// Debug.Log (radianAngle);
-
-							Vector2 newDirection = new Vector2 (Mathf.Sin (radianAngle),Mathf.Cos (radianAngle)) * rb2d.velocity.magnitude;
-							// * rb2d.velocity.magnitude
-
-							// Debug("Current Velocity:
-							rb2d.velocity = newDirection;
+							Bounce (gameObject, 50f);
 
 							lastBounceTime = Time.time;
-							// bounceAudio.Play ();
-							// Bounce
+
+
 						}
 
 
@@ -68,4 +56,43 @@ public class BounceByTags : MonoBehaviour {
 		} // END bouncing enabled check
 			
 	} // end OnTriggerEnter2D
+
+
+	public static void Bounce(GameObject bouncing,float bounceDistance) {
+
+		Rigidbody2D rb2d = bouncing.GetComponent<Rigidbody2D> ();
+		// Debug.Log (name+" with tag "+tag+"is off map");
+		// player.transform.position = Vector3.zero;
+
+		float bouncedX = bouncing.transform.position.x;
+		float bouncedY = bouncing.transform.position.y;
+
+		// Bounce off
+		if (Mathf.Abs (bouncedX) > Mathf.Abs (bouncedY)) {
+			//bounceDistance = Mathf.Abs(player.transform.position.x * 0.1f);
+			bouncedX = bouncedX - Mathf.Sign (bouncedY) * bounceDistance;
+
+		} else {
+			//bounceDistance = Mathf.Abs(player.transform.position.y * 0.1f);
+			bouncedY = bouncedY - Mathf.Sign (bouncedY) * bounceDistance;
+			// Debug.Log (bouncedY);
+		}
+
+
+		bouncing.transform.position = new Vector3 (bouncedX, bouncedY, 0.0f);
+
+
+		bouncing.transform.Rotate (0, 0, 180);
+
+
+
+		float radianAngle = -1 * (Mathf.Deg2Rad * (bouncing.transform.eulerAngles.z - 360.0f + Random.Range (0, 90)));
+
+		Vector2 newDirection = new Vector2 (Mathf.Sin (radianAngle),Mathf.Cos (radianAngle)) * rb2d.velocity.magnitude;
+		// * rb2d.velocity.magnitude
+
+		// Debug("Current Velocity:
+		rb2d.velocity = newDirection;
+
+	}
 }
--- Assets/Scripts/CheckNotOffMap.cs
+++ Assets/Scripts/CheckNotOffMap.cs
@@ -5,13 +5,7 @@ public class CheckNotOffMap : MonoBehaviour {
 
 	public int halfMapSide = 1024;
 
-	private Rigidbody2D rb2d;
 
-	// Use this for initialization
-	void Start () {
-		rb2d = GetComponent<Rigidbody2D> ();
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -42,42 +36,12 @@ public class CheckNotOffMap : MonoBehaviour {
 		}
 
 		if (isOffMap) {
-
-			// Debug.Log (name+" with tag "+tag+"is off map");
-				// player.transform.position = Vector3.zero;
-
-				float bounceDistance = 10.0f;
-				float bouncedX = transform.position.x;
-				float bouncedY = transform.position.y;
-
-				// Bounce off
-				if (Mathf.Abs (transform.position.x) > Mathf.Abs (transform.position.y)) {
-					//bounceDistance = Mathf.Abs(player.transform.position.x * 0.1f);
-					bouncedX = transform.position.x - Mathf.Sign (transform.position.x) * bounceDistance;
-
-				} else {
-					//bounceDistance = Mathf.Abs(player.transform.position.y * 0.1f);
-					bouncedY = transform.position.y - Mathf.Sign (transform.position.y) * bounceDistance;
-					// Debug.Log (bouncedY);
-				}
-
-
-				transform.position = new Vector3 (bouncedX, bouncedY, 0.0f);
-				transform.Rotate (0, 0, 180);
-			float radianAngle = -1*(Mathf.Deg2Rad * (transform.eulerAngles.z-360.0f+Random.Range(0,90)));
-			// Debug.Log (radianAngle);
-
-			Vector2 newDirection = new Vector2 (Mathf.Sin (radianAngle),Mathf.Cos (radianAngle)) * rb2d.velocity.magnitude;
-			// * rb2d.velocity.magnitude
-
-			// Debug("Current Velocity:
-			rb2d.velocity = newDirection;
-
-
-	
+			BounceByTags.Bounce (gameObject,100);
 		} 
 
 	}
+
+
 
 
 }
